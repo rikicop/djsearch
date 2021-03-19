1c90da5f8f9dddd14da120497560f5f976937753
rikicop <ruperto1@protonmail.com>
2021-03-18 21:43:39 -0400

    Render the DataFrame

--- website/views.py
+++ website/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from .models import Inmueble
 import pandas as pd
+import json 
 
 def home(request):
     if request.method =="POST":
@@ -15,36 +16,81 @@ def home(request):
 
         if (tipo_form != '') and (ubicacion_form != '') and (edo_form != ''): 
             newdf = df.loc[((df.tipo == tipo_form) & (df.ubicacion == ubicacion_form) & (df.edo == edo_form))]
+            json_records = newdf.reset_index().to_json(orient ='records')
+            data = []
+            #data = newdf.to_json(orient='records')
+            #print(data)
             print(newdf)
+            data = json.loads(json_records) 
+            context = {'d': data} 
+            
 
         elif (tipo_form != '') and (ubicacion_form != '') and (edo_form == ''): 
             newdf = df.loc[((df.tipo == tipo_form) & (df.ubicacion == ubicacion_form))]
+            json_records = newdf.reset_index().to_json(orient ='records')
+            data = []
+            #data = newdf.to_json(orient='records')
+            #print(data)
             print(newdf)
+            data = json.loads(json_records) 
+            context = {'d': data} 
 
         elif (tipo_form != '') and (ubicacion_form == '') and (edo_form == ''): 
             newdf = df.loc[df.tipo == tipo_form]
+            json_records = newdf.reset_index().to_json(orient ='records')
+            data = []
+            #data = newdf.to_json(orient='records')
+            #print(data)
             print(newdf)
+            data = json.loads(json_records) 
+            context = {'d': data} 
 
         elif (tipo_form == '') and (ubicacion_form != '') and (edo_form == ''): 
             newdf = df.loc[df.ubicacion == ubicacion_form]
+            json_records = newdf.reset_index().to_json(orient ='records')
+            data = []
+            #data = newdf.to_json(orient='records')
+            #print(data)
             print(newdf)
+            data = json.loads(json_records) 
+            context = {'d': data} 
 
         elif (tipo_form == '') and (ubicacion_form != '') and (edo_form != ''):
             newdf = df.loc[((df.ubicacion == ubicacion_form) & (df.edo == edo_form))]
+            json_records = newdf.reset_index().to_json(orient ='records')
+            data = []
+            #data = newdf.to_json(orient='records')
+            #print(data)
             print(newdf)
+            data = json.loads(json_records) 
+            context = {'d': data} 
 
         elif (tipo_form != '') and (ubicacion_form == '') and (edo_form != ''): 
             newdf = df.loc[((df.tipo == tipo_form) & (df.edo == edo_form))]
+            json_records = newdf.reset_index().to_json(orient ='records')
+            data = []
+            #data = newdf.to_json(orient='records')
+            #print(data)
             print(newdf)
+            data = json.loads(json_records) 
+            context = {'d': data} 
 
         elif (tipo_form == '') and (ubicacion_form == '') and (edo_form != ''): 
             newdf = df.loc[df.edo == edo_form]
+            json_records = newdf.reset_index().to_json(orient ='records')
+            data = []
+            #data = newdf.to_json(orient='records')
+            #print(data)
             print(newdf)
+            data = json.loads(json_records) 
+            context = {'d': data} 
         
+        #context = {'data': data} 
+        #return render(request, 'home.html',{'items':items ,'tipo_form' : tipo_form, 'ubicacion_form' : ubicacion_form})
+        return render(request, 'home.html', context)
 
-        return render(request, 'home.html',{'items':items ,'tipo_form' : tipo_form, 
-        'ubicacion_form' : ubicacion_form})
     else:
+
         return render(request, 'home.html')
     
     return render(request, 'home.html')
